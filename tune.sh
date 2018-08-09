6537ce9a8b80714a245c57a6ad7f3d3dac07f522
Ken Kuo <ken.kuo@easystack.cn>
2018-08-09 10:26:06 +0800

    tmp

--- tune.sh
+++ tune.sh
@@ -2,25 +2,81 @@
 MEMCG_DIR="/sys/fs/cgroup/memory/kubepods"
 MEM_LIMIT_FILES=$(find /sys/fs/cgroup/memory/kubepods -name memory.limit_in_bytes)
 RESET=0
+MEM_HIGH_RATIO=0.8
+INT_MAX=9223372036854771712
+DATE_PRINTED=0
 
-date
+FUNC=$1
 
-for MEM_LIMIT_FILE in $MEM_LIMIT_FILES
-do
-	MEMCG_SUB_DIR=$(dirname $MEM_LIMIT_FILE)
-	MEM_LIMIT=$(cat $MEM_LIMIT_FILE)
-	if (( $RESET == 1 &&
-		$(cat $MEMCG_SUB_DIR/memory.high) < 9223372036854771712 )); then
-		echo -1 > $MEMCG_SUB_DIR/memory.high
-	elif (( $RESET == 0 &&
-		$MEM_LIMIT < 9223372036854771712 &&
-		$(cat $MEMCG_SUB_DIR/memory.high) == 9223372036854771712 )); then
-		awk "BEGIN {print int($MEM_LIMIT * 0.8); exit}" > $MEMCG_SUB_DIR/memory.high
-	else
-		continue
+function print_date(){
+	if [[ "$DATE_PRINTED" == "0" ]];then
+		date
+		DATE_PRINTED=1
 	fi
-	grep -H "" $MEM_LIMIT_FILE
-	grep -H "" $MEMCG_SUB_DIR/memory.high
-done
+}
 
-echo ""
+function mem_high_set(){
+	for MEM_LIMIT_FILE in $MEM_LIMIT_FILES
+	do
+		MEMCG_SUB_DIR=$(dirname $MEM_LIMIT_FILE)
+		MEM_LIMIT=$(cat $MEM_LIMIT_FILE)
+		if (( $MEM_LIMIT < $INT_MAX &&
+			$(cat $MEMCG_SUB_DIR/memory.high) == $INT_MAX )); then
+			awk "BEGIN {print int($MEM_LIMIT * $MEM_HIGH_RATIO); exit}" > $MEMCG_SUB_DIR/memory.high
+		else
+			continue
+		fi
+		print_date
+		grep -H "" $MEM_LIMIT_FILE
+		grep -H "" $MEMCG_SUB_DIR/memory.high
+	done
+}
+
+function mem_high_clear(){
+	for MEM_LIMIT_FILE in $MEM_LIMIT_FILES
+	do
+		MEMCG_SUB_DIR=$(dirname $MEM_LIMIT_FILE)
+		if (( $(cat $MEMCG_SUB_DIR/memory.high) < $INT_MAX )); then
+			echo -1 > $MEMCG_SUB_DIR/memory.high
+		else
+			continue
+		fi
+		print_date
+		grep -H "" $MEM_LIMIT_FILE
+		grep -H "" $MEMCG_SUB_DIR/memory.high
+	done
+}
+
+function show(){
+	for MEM_LIMIT_FILE in $MEM_LIMIT_FILES
+	do
+		MEMCG_SUB_DIR=$(dirname $MEM_LIMIT_FILE)
+		MEM_LIMIT=$(cat $MEM_LIMIT_FILE)
+		if (( $MEM_LIMIT < $INT_MAX )); then
+			grep -H "" $MEM_LIMIT_FILE
+			grep -H "" $MEMCG_SUB_DIR/memory.high
+			grep -H "" $MEMCG_SUB_DIR/memory.usage_in_bytes
+		else
+			continue
+		fi
+	done
+}
+
+case "$FUNC" in
+	"set")
+		mem_high_set
+		;;
+	"clear")
+		mem_high_clear
+		;;
+	"show")
+		show
+		;;
+	*)
+		;;
+esac
+
+if [[ "$DATE_PRINTED" == "1" ]];then
+	echo ""
+fi
+
